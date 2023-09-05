b297358c853b2b5c0c5bf069752c06d272061624
artog1809 <80547175+artog1809@users.noreply.github.com>
2023-09-05 16:55:48 +0300

    realized adding of tasks

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Находим элементы на странице 
+
+const form = document.querySelector("#form");
+
+const taskInput = document.querySelector("#taskInput");
+
+const tasksList = document.querySelector("#tasksList");
+
+form.addEventListener('submit', function (event) {
+    // Отменяем отправку формы
+    event.preventDefault();
+
+    // Достаем текст задачи из поля ввода
+    const taskText = taskInput.value;
+
+    // Формируем разметку для новой задачи
+    const taskHtml = `
+                        <li class="list-group-item d-flex justify-content-between task-item">
+                        <span class="task-title">${taskText}</span>
+                        <div class="task-item__buttons">
+                            <button type="button" data-action="done" class="btn-action">
+                                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+                            </button>
+                            <button type="button" data-action="delete" class="btn-action">
+                                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+                            </button>
+                            </div>
+                            </li>
+                        `
+
+    // Добавляем задачу на стрвницу
+    tasksList.insertAdjacentHTML('beforeend', taskHtml);
+})
